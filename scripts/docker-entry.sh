8dbe94399dc7521a2af618a0ca9868f80492495b
Mikhail Mangushev <mishamx@gmail.com>
2018-05-14 13:16:15 +0300

    Update docker-entry.sh

--- scripts/docker-entry.sh
+++ scripts/docker-entry.sh
@@ -9,4 +9,10 @@ rm -rf /var/spool/cron/crontabs && mkdir -m 0644 -p /var/spool/cron/crontabs
 
 chmod -R 0644 /var/spool/cron/crontabs
 
+env | while read -r LINE; do
+    IFS="=" read VAR VAL <<< ${LINE}
+    sed --in-place "/^${VAR}/d" /etc/security/pam_env.conf || true
+    echo "${VAR} DEFAULT=\"${VAL}\"" >> /etc/security/pam_env.conf
+done
+
 exec "$@"
